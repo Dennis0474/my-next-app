257bb6af0b6b67e12fc2590630f399f5062e4110
Dennis Omoyibo <dennisegheosaomoyibo@gmail.com>
2026-06-05 04:37:27 +0100

    add home and about pages with tailwind styling

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <h1 className="text-4xl font-bold text-blue-600 mb-4">About Page</h1>
+    <p className="text-gray-600 text-lg">This is my about page!</p>
+    </div>
+  );
+}
